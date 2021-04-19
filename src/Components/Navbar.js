bca3c99098b04b21d85633abbab19dab5a8e5d7e
LittleRedButton <ritz1484@cs.com>
2021-04-19 16:30:44 -0600

    Added Nav Bar and Styling

---
+++ src/Components/Navbar.js
@@ -0,0 +1,61 @@
+import React from "react"
+import myLogo from "../myLogo.png"
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+    <div className="container">
+    <div className="logo-container">
+        <a className="navbar-brand" href="#"><img src={myLogo} className="logo" alt="Marissa Nolan" /></a>
+    </div>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+    </button>
+
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ">
+            <li className="nav-item active">
+                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Portfolio</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Blog</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Artwork</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">About</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Resume</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Contact</a>
+            </li>
+        
+        </ul>
+    </div>
+    </div>
+    </nav>
+    )
+}
+
+export default Navbar
+
+/* <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown
+            </a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a className="dropdown-item" href="#">Action</a>
+            <a className="dropdown-item" href="#">Another action</a>
+            <div className="dropdown-divider"></div>
+            <a className="dropdown-item" href="#">Something else here</a>
+            </div>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link disabled" href="#">Disabled</a>
+        </li> */
